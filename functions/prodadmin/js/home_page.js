48d8cf89fb34747274df5656d1de07d694dd98eb
SeanAlexWall <seanalexwall@gmail.com>
2020-02-03 21:27:43 -0600

    add page part 1

--- functions/prodadmin/js/home_page.js
+++ functions/prodadmin/js/home_page.js
@@ -1,3 +1,22 @@
-function home_page(){
+function home_page() {
+    home_page_secured();
+}
+
+function home_page_secured() {
     gblPageContent.innerHTML = `<h1>HOME PAGE</h1>`
+    gblPageContent.innerHTML += `
+      <a href='/add' class="btn btn-outline-primary">Add A Product</a>
+      <a href='/show' class="btn btn-outline-primary">Show Products</a>
+      <button class="btn btn-outline-danger" type="button" onclick="logOut()">Log out</button>
+    `;
+}
+
+async function logOut() {
+    try {
+        await firebase.auth().signOut();
+        window.location.href = '/login';
+    }
+    catch (e) {
+        window.location.href = '/login';
+    }
 }
